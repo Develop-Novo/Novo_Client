6d3086e725e1b6a61a2e4d6e2c7e90388cfd6c4c
yohan11 <yipco77@naver.com>
2023-07-26 11:12:29 +0900

    edit: 메인화면 api 연결

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,44 +2,47 @@ import React, { useEffect, useState } from "react";
 import styles from "./MainPage.module.css";
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/LargeBanner";
-import BannerButton from "../../components/Button/Button";
 import Ranking from "../../components/Ranking/Ranking";
 import Footer from "../../components/Footer/Footer";
 import Button from "../../components/Button/Button";
 import axios from "axios";
 
-//서영수정/////////////////////////////////
 interface NovelInfo {
 	rankingNum: number;
 	novelImage: string;
 	novelTitle: string;
 	novelRating: number;
 }
-//////////////////////////////////////////
+interface BannerInfo {
+	bannerImage: string;
+	bannerTitle: string;
+	bannerSubtitle: string;
+}
+
 const MainPage = () => {
-	//서영수정/////////////////////////////////
-	//빈 배열일 때 문제가 발생하는 것 같아서 수정했습니다..!
+	//랭킹 작품 api연결
 	const [novoNovels, setNovoNovels] = useState<NovelInfo[]>([]);
-	//////////////////////////////////////////
+
 	useEffect(() => {
 		async function fetchData() {
 			try {
 				const novels = [];
 
-				const response = await axios.get(
-					`http://35.216.73.185:8080/content/id/1`
-				);
+				for (var contentId = 1; contentId < 6; contentId++) {
+					const response = await axios.get(
+						`http://35.216.73.185:8080/content/id/${contentId}`
+					);
 
-				const rankingData = {
-					novelTitle: response.data.data.title,
-					novelRating: response.data.data.rating,
-					rankingNum: response.data.data.id,
-					novelImage:
-						"https://github.com/Develop-Novo/Novo_Client/assets/40304565/0a7532ab-ce13-4405-8a90-16a87ed02756",
-				};
+					const rankingData = {
+						novelTitle: response.data.data.title,
+						novelRating: response.data.data.rating,
+						rankingNum: response.data.data.id,
+						novelImage:
+							"https://github.com/ITKOO/connect-dku-client/assets/40304565/382ab3d9-ef2f-4c8c-8aa7-639569ba146d",
+					};
 
-				novels.push(rankingData);
-				
+					novels.push(rankingData);
+				}
 				setNovoNovels(novels);
 			} catch (error) {
 				console.log(error);
@@ -48,30 +51,38 @@ const MainPage = () => {
 
 		fetchData();
 	}, []);
+	//배너 작품 api연결
+	const [bannerList, setBannerList] = useState<BannerInfo[]>([]);
 
-	console.log(novoNovels);
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const banners = [];
 
-	const [bannerList, setBannerList] = useState([
-		{
-			bannerImage:
-				"https://github.com/Develop-Novo/Novo_Client/assets/40304565/0a7532ab-ce13-4405-8a90-16a87ed02756",
-			bannerTitle: "더는 당신의 악녀로 살지 않겠습니다",
-			bannerSubtitle:
-				"괴물이라 손가락질 받았으니 진짜 괴물이 되어주겠어.",
-		},
-		{
-			bannerImage:
-				"https://github.com/Develop-Novo/Novo_Client/assets/40304565/203a6943-f890-4da1-897b-abdb1889f387",
-			bannerTitle: "데뷔 못하면 죽는 병 걸림",
-			bannerSubtitle: "데뷔 못하면 죽는 병 걸림 진짜로",
-		},
-		{
-			bannerImage:
-				"https://github.com/Develop-Novo/Novo_Client/assets/40304565/b40b23d1-04f6-43f8-b2b6-ce8c2968c9d4",
-			bannerTitle: "던전팜!",
-			bannerSubtitle: "던전던전팜팜22",
-		},
-	]);
+				for (var contentId = 1; contentId < 4; contentId++) {
+					const response = await axios.get(
+						`http://35.216.73.185:8080/content/id/${contentId}`
+					);
+
+					const bannerData = {
+						bannerImage:
+							"https://github.com/Develop-Novo/Novo_Client/assets/40304565/0a7532ab-ce13-4405-8a90-16a87ed02756",
+						bannerTitle: response.data.data.title,
+						bannerSubtitle: response.data.data.introduction,
+					};
+
+					banners.push(bannerData);
+				}
+				setBannerList(banners);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		fetchData();
+	}, []);
+
+	//배너 슬라이드 구현
 	const [currentSlide, setCurrentSlide] = useState(0);
 
 	const handleNextSlide = () => {
@@ -86,7 +97,6 @@ const MainPage = () => {
 	};
 
 	useEffect(() => {
-		// Apply transition style to the slideList element
 		const slideListElement = document.getElementById(styles.slideList);
 		if (slideListElement != null) {
 			slideListElement.style.transition = "transform 0.3s ease-in-out";
@@ -94,7 +104,6 @@ const MainPage = () => {
 				currentSlide * 100
 			}%)`;
 
-			// Clean up the transition style after the animation is finished
 			const handleTransitionEnd = () => {
 				slideListElement.style.transition = "";
 			};
@@ -104,7 +113,6 @@ const MainPage = () => {
 			);
 
 			return () => {
-				// Clean up the event listener
 				slideListElement.removeEventListener(
 					"transitionend",
 					handleTransitionEnd
@@ -138,39 +146,41 @@ const MainPage = () => {
 	console.log("rankingList:", rankingList);
 
 	return (
-		novoNovels && <>
-			<Header />
+		novoNovels && (
+			<>
+				<Header />
 
-			<div id={styles.banner_container}>
-				<Button
-					buttonType="left"
-					buttonTop="382px"
-					buttonLeft="201px"
-					onClick={handlePreviousSlide}
-				/>
-				<div id={styles.slideList}>
-					{bannerList.map((item, index) => (
-						<span key={index} className={styles.banner}>
-							<Banner bannerProps={item} />
-						</span>
-					))}
+				<div id={styles.banner_container}>
+					<Button
+						buttonType="left"
+						buttonTop="382px"
+						buttonLeft="201px"
+						onClick={handlePreviousSlide}
+					/>
+					<div id={styles.slideList}>
+						{bannerList.map((item, index) => (
+							<span key={index} className={styles.banner}>
+								<Banner bannerProps={item} />
+							</span>
+						))}
+					</div>
+					<Button
+						buttonType="right"
+						buttonTop="382px"
+						buttonLeft="1683px"
+						onClick={handleNextSlide}
+					/>
 				</div>
-				<Button
-					buttonType="right"
-					buttonTop="382px"
-					buttonLeft="1683px"
-					onClick={handleNextSlide}
-				/>
-			</div>
-			<span className={styles.showSlideNum}>
-				{currentSlide + 1} / {bannerList.length}
-			</span>
+				<span className={styles.showSlideNum}>
+					{currentSlide + 1} / {bannerList.length}
+				</span>
 
-			{rankingList.map((item, index) => (
-				<Ranking key={index} rankingProps={item} /> //여기에서 props넘겨줄 때 타입이 안맞아서 계속 오류가 나요ㅠㅠ 일단 console.log로 테스트 데이터 확인해보시라구 주석처리 해놨습니당!
-			))}
-			<Footer />
-		</>
+				{rankingList.map((item, index) => (
+					<Ranking key={index} rankingProps={item} />
+				))}
+				<Footer />
+			</>
+		)
 	);
 };
 
