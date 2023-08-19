3d6263f19895aa4c2453ef8046795b14729550c9
seoyoung927 <seoyoung0000@naver.com>
2023-08-19 15:12:24 +0900

    edit: 로그인, 회원가입 페이지 -  서버 ip 변경, css 변경

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -5,166 +5,179 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 interface IData {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 function LoginPage() {
-	const [popupOpen, setPopupOpen] = useState(false);
-	const [popupMsg, setPopupMsg] = useState("");
-	////////////////
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		setError,
-		watch
-	} = useForm<IData>({ mode: "onChange" });
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [popupMsg, setPopupMsg] = useState("");
+  ////////////////
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    watch,
+  } = useForm<IData>({ mode: "onChange" });
 
-	const onValid = (data: IData) => {
-		//console.log("Backend에 전송");
-		const postLogin = async () => {
-			try {
-				const response = await axios.post('http://35.216.73.185:8080/member/login', {
-					email: data.email,
-					password: data.password
-				}, {
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				})
-				setPopupMsg("로그인이 완료되었습니다!");
-				console.log(response);
-			} catch (error) {
-				setPopupMsg("비밀번호가 일치하지 않습니다.");
-				console.log(error);
-			}
-		};
-		postLogin();
-		setPopupOpen(true);
-	};
+  const onValid = (data: IData) => {
+    //console.log("Backend에 전송");
+    const postLogin = async () => {
+      try {
+        const response = await axios.post(
+          "http://52.78.121.235:8080/member/login",
+          {
+            email: data.email,
+            password: data.password,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setPopupMsg("로그인이 완료되었습니다!");
+        console.log(response);
+      } catch (error) {
+        setPopupMsg("비밀번호가 일치하지 않습니다.");
+        console.log(error);
+      }
+    };
+    postLogin();
+    setPopupOpen(true);
+  };
 
-	return (
-		<>
-			<div className={styles.container__wrapper}>
-				<div className={styles.container}>
-					<form
-						className={styles.form}
-						onSubmit={handleSubmit(onValid)}
-					>
-						<img
-							className={styles.novo__logo}
-							src="/images/novo__logo.png"
-							alt="novo__logo"
-						/>
-						<h1 id={styles.form__title}>로그인</h1>
-						<div
-							className={styles.input__wrapper}
-							id={styles.input__wrapper__email}>
-							<input
-								className={styles.input}
-								{...register("email", {
-									required: "정확하지 않은 아이디입니다.",
-								})}
-								placeholder="이메일"
-								type="email"
-							/>
-							{errors?.email ? <div className={styles.input__error}>☒</div> :
-								watch('email')&&(watch('email')?.length!==0) ? <div className={styles.input__valid}>☑</div> : null}
-						</div>
-						{errors?.email && (
-							<div
-								className={styles.error__message}
-								id={styles.error__message__email}
-							>
-								{errors?.email?.message}
-							</div>
-						)}
-						<div
-							className={styles.input__wrapper}
-							id={styles.input__wrapper__password}>
-							<input
-								className={styles.input}
-								{...register("password", { required: "정확하지 않은 비밀번호입니다.",
-									minLength: {
-										value: 6,
-										message:
-											"비밀번호는 최소 6자리 이상입니다.",
-									}, })}
-								placeholder="비밀번호"
-								type="password"
-							/>
-							{errors?.password ? <div className={styles.input__error}>☒</div> :
-								watch('password')&&(watch('password')?.length!==0) ? <div className={styles.input__valid}>☑</div> : null}
-						</div>
-						{errors?.password && (
-							<div
-								className={styles.error__message}
-								id={styles.error__message__password}
-							>
-								{errors?.password?.message}
-							</div>
-						)}
-						<button className={styles.button__submit} type="submit">
-							<div className={styles.button__submit__text}>
-								로그인
-							</div>
-						</button>
-						<Link to={`${process.env.PUBLIC_URL}/resetpassword`}>
-							<div className={styles.reset__password__link}>
-								비밀번호를 잃어버리셨나요?
-							</div>
-						</Link>
-						<Link to={`${process.env.PUBLIC_URL}/register`}>
-							<div className={styles.register__link}>
-								계정이 없으신가요?{" "}
-								<span id={styles.register__link__highlight}>
-									회원가입
-								</span>
-							</div>
-						</Link>
-						<hr className={styles.form__hr} />
-						<div className={styles.form__hr__or__text}>OR</div>
-						<div className={styles.logos}>
-							<img
-								className={styles.logo}
-								src="/images/naver__icon.png"
-								alt="naver__icon"
-							/>
-							<img
-								className={styles.logo}
-								src="/images/kakao__icon.png"
-								alt="kakao__icon"
-							/>
-							<img
-								className={styles.logo}
-								src="/images/google__icon.png"
-								alt="google__icon"
-							/>
-							<img
-								className={styles.logo}
-								src="/images/twitter__icon.png"
-								alt="twitter__icon"
-							/>
-							<img
-								className={styles.logo}
-								src="/images/apple__icon.png"
-								alt="apple__icon"
-							/>
-						</div>
-					</form>
-				</div>
-				{popupOpen && <>
-					<div className={styles.cover} onClick={() => setPopupOpen(false)} />
-					<div className={styles.popup}>
-						<div className={styles.success__message}>{popupMsg}</div>
-						<div className={styles.popup__hr} />
-						<span className={styles.popup__button} onClick={() => setPopupOpen(false)}>닫기</span>
-					</div>
-				</>}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={styles.container__wrapper}>
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={handleSubmit(onValid)}>
+            <img
+              className={styles.novo__logo}
+              src="/images/novo__logo.png"
+              alt="novo__logo"
+            />
+            <h1 id={styles.form__title}>로그인</h1>
+            <div
+              className={styles.input__wrapper}
+              id={styles.input__wrapper__email}
+            >
+              <input
+                className={styles.input}
+                {...register("email", {
+                  required: "정확하지 않은 아이디입니다.",
+                })}
+                placeholder="이메일"
+                type="email"
+              />
+              {errors?.email ? (
+                <div className={styles.input__error}>☒</div>
+              ) : watch("email") && watch("email")?.length !== 0 ? (
+                <div className={styles.input__valid}>☑</div>
+              ) : null}
+            </div>
+            {errors?.email && (
+              <div
+                className={styles.error__message}
+                id={styles.error__message__email}
+              >
+                {errors?.email?.message}
+              </div>
+            )}
+            <div
+              className={styles.input__wrapper}
+              id={styles.input__wrapper__password}
+            >
+              <input
+                className={styles.input}
+                {...register("password", {
+                  required: "정확하지 않은 비밀번호입니다.",
+                  minLength: {
+                    value: 6,
+                    message: "비밀번호는 최소 6자리 이상입니다.",
+                  },
+                })}
+                placeholder="비밀번호"
+                type="password"
+              />
+              {errors?.password ? (
+                <div className={styles.input__error}>☒</div>
+              ) : watch("password") && watch("password")?.length !== 0 ? (
+                <div className={styles.input__valid}>☑</div>
+              ) : null}
+            </div>
+            {errors?.password && (
+              <div
+                className={styles.error__message}
+                id={styles.error__message__password}
+              >
+                {errors?.password?.message}
+              </div>
+            )}
+            <button className={styles.button__submit} type="submit">
+              <div className={styles.button__submit__text}>로그인</div>
+            </button>
+            <Link to={`${process.env.PUBLIC_URL}/resetpassword`}>
+              <div className={styles.reset__password__link}>
+                비밀번호를 잃어버리셨나요?
+              </div>
+            </Link>
+            <Link to={`${process.env.PUBLIC_URL}/register`}>
+              <div className={styles.register__link}>
+                계정이 없으신가요?{" "}
+                <span id={styles.register__link__highlight}>회원가입</span>
+              </div>
+            </Link>
+            <hr className={styles.form__hr} />
+            <div className={styles.form__hr__or__text}>OR</div>
+            <div className={styles.logos}>
+              <img
+                className={styles.logo}
+                src="/images/naver__icon.png"
+                alt="naver__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/kakao__icon.png"
+                alt="kakao__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/google__icon.png"
+                alt="google__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/twitter__icon.png"
+                alt="twitter__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/apple__icon.png"
+                alt="apple__icon"
+              />
+            </div>
+          </form>
+        </div>
+        {popupOpen && (
+          <>
+            <div className={styles.cover} onClick={() => setPopupOpen(false)} />
+            <div className={styles.popup}>
+              <div className={styles.success__message}>{popupMsg}</div>
+              <div className={styles.popup__hr} />
+              <span
+                className={styles.popup__button}
+                onClick={() => setPopupOpen(false)}
+              >
+                닫기
+              </span>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,136 +1,216 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import styles from "./RegisterPage.module.css";
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 interface IData {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 function RegisterPage() {
-    const [popupOpen, setPopupOpen] = useState(false);
-    //nativgate/////
-    const navigate = useNavigate();
-    //regex/////////
-    const regex = /^(?=.*[A-Za-z])(?=.*\d|.*[\W_]).{6,}$/;
-    ////////////////
-    const { register, handleSubmit, formState: { errors }, watch } = useForm<IData>({ mode: "onChange" });
-    const onValid = (data: IData) => {
-        console.log(data.password);
-        console.log(regex.test(data.password));
-        //console.log("Backend에 전송");
-        const postMember = async () => {
-            try {
-                const response = await axios.post('http://35.216.73.185:8080/member/new', {
-                    name: data.name,
-                    email: data.email,
-                    password: data.password
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                console.log(response);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        postMember();
-        setPopupOpen(true);
-    }
+  const [popupOpen, setPopupOpen] = useState(false);
+  //nativgate/////
+  const navigate = useNavigate();
+  //regex/////////
+  const regex = /^(?=.*[A-Za-z])(?=.*\d|.*[\W_]).{6,}$/;
+  ////////////////
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<IData>({ mode: "onChange" });
+  const onValid = (data: IData) => {
+    console.log(data.password);
+    console.log(regex.test(data.password));
+    //console.log("Backend에 전송");
+    const postMember = async () => {
+      try {
+        const response = await axios.post(
+          "http://52.78.121.235:8080/member/new",
+          {
+            name: data.name,
+            email: data.email,
+            password: data.password,
+            authority: "ROLE_USER",
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postMember();
+    setPopupOpen(true);
+  };
 
-    return <>
-        <div className={styles.container__wrapper}>
-            <div className={styles.container}>
-                <form className={styles.form} onSubmit={handleSubmit(onValid)}>
-                    <img className={styles.novo__logo} src="/images/novo__logo.png" alt="novo__logo" />
-                    <h1 id={styles.form__title}>
-                        회원가입
-                    </h1>
-                    <div
-                        className={styles.input__wrapper}
-                        id={styles.input__wrapper__name}>
-                        <input className={styles.input}
-                            {...register("name", { required: "정확하지 않은 이름입니다." })}
-                            placeholder="이름"
-                            type="text"
-                        />
-                        {errors?.name ? <div className={styles.input__error}>☒</div> :
-                            watch('name') && (watch('name')?.length !== 0) ? <div className={styles.input__valid}>☑</div> : null}
-
-                    </div>
-                    {errors?.name && <div className={styles.error__message} id={styles.error__message__name}>{errors?.name?.message}</div>}
-
-                    <div
-                        className={styles.input__wrapper}
-                        id={styles.input__wrapper__email}>
-                        <input className={styles.input} id={styles.input__email}
-                            {...register("email", { required: "정확하지 않은 이메일입니다." })}
-                            placeholder="이메일"
-                            type="email"
-                        />
-                        {errors?.email ? <div className={styles.input__error}>☒</div> :
-                            watch('email') && (watch('email')?.length !== 0) ? <div className={styles.input__valid}>☑</div> : null}
-
-                    </div>
-                    {errors?.email && <div className={styles.error__message} id={styles.error__message__email}>{errors?.email?.message}</div>}
-
-                    <div
-                        className={styles.input__wrapper}
-                        id={styles.input__wrapper__password}>
-                        <input className={styles.input} id={styles.input__password}
-                            {...register("password", 
-                            { required: "정확하지 않은 비밀번호입니다.",
-                            pattern:{
-                                value: /^(?=.*[A-Za-z])(?=.*\d|.*[\W_]).{6,}$/,
-                                message: '비밀번호는 영문, 숫자, 특수문자 중 2개 이상을 조합하여 최소 6자리 이상이어야 합니다.',
-                            } })}
-                            placeholder="비밀번호"
-                            type="password"
-                        />
-                        {errors?.password ? <div className={styles.input__error}>☒</div> :
-                            watch('password') && (watch('password')?.length !== 0) ? <div className={styles.input__valid}>☑</div> : null}
-                    </div>
-                    {errors?.password && <div className={styles.error__message} id={styles.error__message__password}>{errors?.password?.message}</div>}
-                    <button className={styles.button__submit} type="submit">
-                        <div className={styles.button__submit__text}>
-                            회원가입
-                        </div>
-                    </button>
-                    <Link to={`${process.env.PUBLIC_URL}/`}>
-                        <div className={styles.login__link}>
-                            이미 가입하셨나요? <span id={styles.login__link__highlight}>로그인</span>
-                        </div>
-                    </Link>
-
-                    <hr className={styles.form__hr} />
-                    <div className={styles.form__hr__or__text}>OR</div>
-                    <div className={styles.logos}>
-                        <img className={styles.logo} src="/images/naver__icon.png" alt="naver__icon" />
-                        <img className={styles.logo} src="/images/kakao__icon.png" alt="kakao__icon" />
-                        <img className={styles.logo} src="/images/google__icon.png" alt="google__icon" />
-                        <img className={styles.logo} src="/images/twitter__icon.png" alt="twitter__icon" />
-                        <img className={styles.logo} src="/images/apple__icon.png" alt="apple__icon" />
-                    </div>
-                </form>
+  return (
+    <>
+      <div className={styles.container__wrapper}>
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={handleSubmit(onValid)}>
+            <img
+              className={styles.novo__logo}
+              src="/images/novo__logo.png"
+              alt="novo__logo"
+            />
+            <h1 id={styles.form__title}>회원가입</h1>
+            <div
+              className={styles.input__wrapper}
+              id={styles.input__wrapper__name}
+            >
+              <input
+                className={styles.input}
+                {...register("name", { required: "정확하지 않은 이름입니다." })}
+                placeholder="이름"
+                type="text"
+              />
+              {errors?.name ? (
+                <div className={styles.input__error}>☒</div>
+              ) : watch("name") && watch("name")?.length !== 0 ? (
+                <div className={styles.input__valid}>☑</div>
+              ) : null}
             </div>
-            {popupOpen && <>
-                <div className={styles.cover} onClick={() => setPopupOpen(false)} />
+            {errors?.name && (
+              <div
+                className={styles.error__message}
+                id={styles.error__message__name}
+              >
+                {errors?.name?.message}
+              </div>
+            )}
+
+            <div
+              className={styles.input__wrapper}
+              id={styles.input__wrapper__email}
+            >
+              <input
+                className={styles.input}
+                id={styles.input__email}
+                {...register("email", {
+                  required: "정확하지 않은 이메일입니다.",
+                })}
+                placeholder="이메일"
+                type="email"
+              />
+              {errors?.email ? (
+                <div className={styles.input__error}>☒</div>
+              ) : watch("email") && watch("email")?.length !== 0 ? (
+                <div className={styles.input__valid}>☑</div>
+              ) : null}
+            </div>
+            {errors?.email && (
+              <div
+                className={styles.error__message}
+                id={styles.error__message__email}
+              >
+                {errors?.email?.message}
+              </div>
+            )}
+
+            <div
+              className={styles.input__wrapper}
+              id={styles.input__wrapper__password}
+            >
+              <input
+                className={styles.input}
+                id={styles.input__password}
+                {...register("password", {
+                  required: "정확하지 않은 비밀번호입니다.",
+                  pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d|.*[\W_]).{6,}$/,
+                    message:
+                      "비밀번호는 영문, 숫자, 특수문자 중 2개 이상을 조합하여 최소 6자리 이상이어야 합니다.",
+                  },
+                })}
+                placeholder="비밀번호"
+                type="password"
+              />
+              {errors?.password ? (
+                <div className={styles.input__error}>☒</div>
+              ) : watch("password") && watch("password")?.length !== 0 ? (
+                <div className={styles.input__valid}>☑</div>
+              ) : null}
+            </div>
+            {errors?.password && (
+              <div
+                className={styles.error__message}
+                id={styles.error__message__password}
+              >
+                {errors?.password?.message}
+              </div>
+            )}
+            <button className={styles.button__submit} type="submit">
+              <div className={styles.button__submit__text}>회원가입</div>
+            </button>
+            <Link to={`${process.env.PUBLIC_URL}/`}>
+              <div className={styles.login__link}>
+                이미 가입하셨나요?{" "}
+                <span id={styles.login__link__highlight}>로그인</span>
+              </div>
+            </Link>
+
+            <hr className={styles.form__hr} />
+            <div className={styles.form__hr__or__text}>OR</div>
+            <div className={styles.logos}>
+              <img
+                className={styles.logo}
+                src="/images/naver__icon.png"
+                alt="naver__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/kakao__icon.png"
+                alt="kakao__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/google__icon.png"
+                alt="google__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/twitter__icon.png"
+                alt="twitter__icon"
+              />
+              <img
+                className={styles.logo}
+                src="/images/apple__icon.png"
+                alt="apple__icon"
+              />
+            </div>
+          </form>
+          {popupOpen && (
+            <>
+              <div
+                className={styles.cover}
+                onClick={() => setPopupOpen(false)}
+              />
+              <div className={styles.popup__wrapper}>
                 <div className={styles.popup}>
-                    <div className={styles.success__message}>회원가입이 완료되었습니다!</div>
-                    <div className={styles.popup__hr} />
-                    <span className={styles.popup__button}>
-                        <Link to={`${process.env.PUBLIC_URL}/`}>
-                            로그인하기
-                        </Link>
-                    </span>
+                  <div className={styles.success__message}>
+                    회원가입이 완료되었습니다!
+                  </div>
+                  <div className={styles.popup__hr} />
+                  <span className={styles.popup__button}>
+                    <Link to={`${process.env.PUBLIC_URL}/`}>로그인하기</Link>
+                  </span>
                 </div>
-            </>}
+              </div>
+            </>
+          )}
         </div>
+      </div>
     </>
+  );
 }
 
 export default RegisterPage;
