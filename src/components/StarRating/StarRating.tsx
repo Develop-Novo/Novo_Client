e5cf180c0d916fc6b4bb8fa7b3e85d27348d19e4
seoyoung927 <seoyoung0000@naver.com>
2023-08-19 15:14:07 +0900

    edit: 별점 오류 수정 - 서버 IP 변경, 평점 반올림, 별점 생성, 수정 오류 해결

--- src/components/StarRating/StarRating.tsx
+++ src/components/StarRating/StarRating.tsx
@@ -3,129 +3,173 @@ import styles from "./StarRating.module.css";
 import axios from "axios";
 
 // StarRating 컴포넌트의 props 타입 정의
-interface IRating{
-    star: number;
-    contentId: number;
-    memberId: number;
-    id: number;
+interface IRating {
+  star: number;
+  contentId: number;
+  memberId: number;
+  id: number;
 }
 interface StarRatingProps {
-	myRating: IRating|null;
+  novelId: number;
+  myRating: IRating | null;
 }
 
-function StarRating({myRating}:StarRatingProps){
-    const [hoveredItem, setHoveredItem] = useState<number | null>(null);
-    const [clickedItem, setClickedItem] = useState<number | null>(myRating ? myRating.star : 0);
-    const handleMouseEnter = (itemId: number) => {
-        setHoveredItem(itemId);
-    };
+function StarRating({ myRating }: StarRatingProps) {
+  const [hoveredItem, setHoveredItem] = useState<number | null>(null);
+  const [clickedItem, setClickedItem] = useState<number | null>(
+    myRating ? myRating.star : 0
+  );
+  const handleMouseEnter = (itemId: number) => {
+    setHoveredItem(itemId);
+  };
 
-    const handleMouseLeave = () => {
-        setHoveredItem(null);
-    };
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
 
-    const handleClick = (itemId: number) => {
-        const postNewStar = async(star:number) => {
-            try {
-				const response = await axios.post('http://35.216.73.185:8080/star/new', {
-                    memberId: 1,
-                    contentId: 1,
-                    star: star,
-                },{
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				})
-				return response.data;
-			} catch (error) {
-				console.log(error);
-			}
-        }
-        const modifyStar = async(star:number, starId: number) => {
-            try {
-				const response = await axios.put(`http://35.216.73.185:8080/star/id/${starId}`, {
-                    memberId: 1,
-                    contentId: 1,
-                    star: star,
-                },{
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				})
-				return response.data;
-			} catch (error) {
-				console.log(error);
-			}
-        }
-        setClickedItem(itemId);
-        if(myRating){
-            modifyStar(itemId, myRating.id);
-        }else{
-            postNewStar(itemId);
-        }
+  const handleClick = (itemId: number) => {
+    const postNewStar = async (star: number) => {
+      try {
+        const response = await axios.post(
+          "http://52.78.121.235:8080/star/new",
+          {
+            memberId: 152,
+            contentId: 1,
+            star: star,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        return response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const modifyStar = async (star: number, starId: number) => {
+      try {
+        const response = await axios.put(
+          `http://52.78.121.235:8080/star/id/${starId}`,
+          {
+            memberId: 152,
+            contentId: 1,
+            star: star,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        return response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    setClickedItem(itemId);
+    if (myRating) {
+      modifyStar(itemId, myRating.id);
+    } else {
+      postNewStar(itemId);
     }
+  };
 
-    //console.log(hoveredItem, clickedItem);
+  //console.log(hoveredItem, clickedItem);
 
-    return (<div className={styles.star__container}>
-        <div className={styles.stars__wrapper}>
-            {[0, 2, 4, 6, 8].map((i) => <div key={i}>
-                <div className={styles.star__wrapper}>
-                    <div className={styles.star__inner__wrapper}>
-                        {(hoveredItem && i + 1 <= hoveredItem) || (clickedItem && i + 1 <= clickedItem) ? <>
-                            <div className={`${styles.star} ${styles.star__left}`} id={styles.star__clicked}
-                                onMouseEnter={() => handleMouseEnter(i + 1)}
-                                onMouseLeave={handleMouseLeave}
-                                onClick={() => handleClick(i + 1)}
-                            >
-                                ★
-                            </div></> : <>
-                            <div className={`${styles.star} ${styles.star__left}`} id={styles.star__nonClicked}
-                                onMouseEnter={() => handleMouseEnter(i + 1)}
-                                onMouseLeave={handleMouseLeave}
-                                onClick={() => handleClick(i + 1)}
-                            >
-                                ★
-                            </div></>
-                        }
+  return (
+    <div className={styles.star__container}>
+      <div className={styles.stars__wrapper}>
+        {[0, 2, 4, 6, 8].map((i) => (
+          <div key={i}>
+            <div className={styles.star__wrapper}>
+              <div className={styles.star__inner__wrapper}>
+                {(hoveredItem && i + 1 <= hoveredItem) ||
+                (clickedItem && i + 1 <= clickedItem) ? (
+                  <>
+                    <div
+                      className={`${styles.star} ${styles.star__left}`}
+                      id={styles.star__clicked}
+                      onMouseEnter={() => handleMouseEnter(i + 1)}
+                      onMouseLeave={handleMouseLeave}
+                      onClick={() => handleClick(i + 1)}
+                    >
+                      ★
                     </div>
-                    <div className={styles.star__inner__wrapper}>
-                        {(hoveredItem && i + 2 <= hoveredItem) || (clickedItem && i + 2 <= clickedItem) ? <>
-                            <div className={`${styles.star} ${styles.star__right}`} id={styles.star__clicked}
-                                onMouseEnter={() => handleMouseEnter(i + 2)}
-                                onMouseLeave={handleMouseLeave}
-                                onClick={() => handleClick(i + 2)}
-                            >
-                                ★
-                            </div></> : <>
-                            <div className={`${styles.star} ${styles.star__right}`} id={styles.star__nonClicked}
-                                onMouseEnter={() => handleMouseEnter(i + 2)}
-                                onMouseLeave={handleMouseLeave}
-                                onClick={() => handleClick(i + 2)}
-                            >
-                                ★
-                            </div></>
-                        }
+                  </>
+                ) : (
+                  <>
+                    <div
+                      className={`${styles.star} ${styles.star__left}`}
+                      id={styles.star__nonClicked}
+                      onMouseEnter={() => handleMouseEnter(i + 1)}
+                      onMouseLeave={handleMouseLeave}
+                      onClick={() => handleClick(i + 1)}
+                    >
+                      ★
                     </div>
-                </div></div>)}
-        </div>
-        <div className={styles.star__message__wrapper}>
-            {!clickedItem ? "평가하기" :
-            clickedItem === 10 ? "최고예요!":
-            clickedItem === 9 ? "훌륭해요!":
-            clickedItem === 8 ? "재미있어요":
-            clickedItem === 7 ? "볼만해요":
-            clickedItem === 6 ? "보통이에요":
-            clickedItem === 5 ? "부족해요":
-            clickedItem === 4 ? "별로예요":
-            clickedItem === 3 ? "재미없어요":
-            clickedItem === 2 ? "싫어요":
-            "최악이에요"}
-        </div>
-        <div className={styles.rating}>
-            ★ {clickedItem}
-        </div>
-    </div>);
+                  </>
+                )}
+              </div>
+              <div className={styles.star__inner__wrapper}>
+                {(hoveredItem && i + 2 <= hoveredItem) ||
+                (clickedItem && i + 2 <= clickedItem) ? (
+                  <>
+                    <div
+                      className={`${styles.star} ${styles.star__right}`}
+                      id={styles.star__clicked}
+                      onMouseEnter={() => handleMouseEnter(i + 2)}
+                      onMouseLeave={handleMouseLeave}
+                      onClick={() => handleClick(i + 2)}
+                    >
+                      ★
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div
+                      className={`${styles.star} ${styles.star__right}`}
+                      id={styles.star__nonClicked}
+                      onMouseEnter={() => handleMouseEnter(i + 2)}
+                      onMouseLeave={handleMouseLeave}
+                      onClick={() => handleClick(i + 2)}
+                    >
+                      ★
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.star__message__wrapper}>
+        {!clickedItem
+          ? "평가하기"
+          : clickedItem === 10
+          ? "최고예요!"
+          : clickedItem === 9
+          ? "훌륭해요!"
+          : clickedItem === 8
+          ? "재미있어요"
+          : clickedItem === 7
+          ? "볼만해요"
+          : clickedItem === 6
+          ? "보통이에요"
+          : clickedItem === 5
+          ? "부족해요"
+          : clickedItem === 4
+          ? "별로예요"
+          : clickedItem === 3
+          ? "재미없어요"
+          : clickedItem === 2
+          ? "싫어요"
+          : "최악이에요"}
+      </div>
+      <div className={styles.rating}>★ {clickedItem}</div>
+    </div>
+  );
 }
 
 export default StarRating;
--- src/components/StarRatingChart/StarRatingChart.tsx
+++ src/components/StarRatingChart/StarRatingChart.tsx
@@ -2,87 +2,100 @@ import { useEffect, useState } from "react";
 import styles from "./StarRatingChart.module.css";
 
 interface barProps {
-    barHeight: string;
-    highlight: boolean;
+  barHeight: string;
+  highlight: boolean;
 }
 function Bar({ barHeight, highlight }: barProps) {
-    const barStyle: React.CSSProperties = {
-        height: barHeight,
-        background: highlight ? "#FF6B01 0% 0% no-repeat padding-box" : "#FFAD76 0% 0% no-repeat padding-box"
-    };
-    return (
-        <>
-            <div className={styles.chart__bar} style={barStyle} />
-        </>
-    );
+  const barStyle: React.CSSProperties = {
+    height: barHeight,
+    background: highlight
+      ? "#FF6B01 0% 0% no-repeat padding-box"
+      : "#FFAD76 0% 0% no-repeat padding-box",
+  };
+  return (
+    <>
+      <div className={styles.chart__bar} style={barStyle} />
+    </>
+  );
 }
 
 interface StarRatingChartProps {
-    ratings: number[];
-    rating: number;
-    count: number;
+  ratings: number[];
+  rating: number;
+  count: number;
 }
 function StarRatingChart({ ratings, rating, count }: StarRatingChartProps) {
-    const [total, setTotal] = useState<number>(0);
-    const [maxIdx, setMaxIdx] = useState<number | null>(null);
-    useEffect(() => {
-        function sumArray(arr: number[]) {
-            let sum = 0;
-            for (let i = 0; i < arr.length; i++) {
-                sum += arr[i];
-            }
-            return sum;
+  const [total, setTotal] = useState<number>(0);
+  const [maxIdx, setMaxIdx] = useState<number | null>(null);
+  useEffect(() => {
+    function sumArray(arr: number[]) {
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+      }
+      return sum;
+    }
+    function getMaxIndex(arr: number[]) {
+      let max = arr[0];
+      let maxIndex = 0;
+
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+          max = arr[i];
+          maxIndex = i;
         }
-        function getMaxIndex(arr: number[]) {
-            let max = arr[0];
-            let maxIndex = 0;
+      }
 
-            for (let i = 1; i < arr.length; i++) {
-                if (arr[i] > max) {
-                    max = arr[i];
-                    maxIndex = i;
-                }
-            }
+      return maxIndex;
+    }
+    setTotal(sumArray(ratings));
+    setMaxIdx(getMaxIndex(ratings));
+  }, []);
 
-            return maxIndex;
-        }
-        setTotal(sumArray(ratings));
-        setMaxIdx(getMaxIndex(ratings));
-    }, []);
-
-    return (<>
-        <div className={styles.chart__wrapper}>
-            <div className={styles.chart__title}>
-                별점그래프
-            </div>
-            <div className={styles.chart__rating__wrapper}>
-                <div id={styles.chart__rating__avg}>평균 ★{rating}</div>
-                <div id={styles.chart__rating__cnt}>({count}명)</div>
-            </div>
-            <div className={styles.chart}>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => i !== 10 ? <div key={i} className={styles.chart__bar__wrapper}>
-                    <div className={styles.chart__bar__inner__wrapper}>
-                        <Bar
-                            highlight={maxIdx ? i === (maxIdx+1) : false}
-                            barHeight={`${ratings[i - 1] / total * 312}px`} />
-                    </div>
-                    <div className={styles.chart__xlabel}>
-                        <p className={styles.chart__xlabel__text}>{`${i}`}</p>
-                    </div>
-                </div> : <div key={i} className={styles.chart__bar__wrapper} id={styles.chart__bar__wrapper__last}>
-                    <div className={styles.chart__bar__inner__wrapper}>
-                        <Bar
-                            highlight={maxIdx ? i === (maxIdx+1) : false}
-                            barHeight={`${ratings[i - 1] / total * 312}px`} />
-                    </div>
-                    <div className={styles.chart__xlabel}>
-                        <p className={styles.chart__xlabel__text}>{`${i}`}</p>
-                    </div>
-                </div>)}
-            </div>
+  return (
+    <>
+      <div className={styles.chart__wrapper}>
+        <div className={styles.chart__title}>별점그래프</div>
+        <div className={styles.chart__rating__wrapper}>
+          <div id={styles.chart__rating__avg}>평균 ★{rating.toFixed(1)}</div>
+          <div id={styles.chart__rating__cnt}>({count}명)</div>
         </div>
-    </>);
+        <div className={styles.chart}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+            i !== 10 ? (
+              <div key={i} className={styles.chart__bar__wrapper}>
+                <div className={styles.chart__bar__inner__wrapper}>
+                  <Bar
+                    highlight={maxIdx ? i === maxIdx + 1 : false}
+                    barHeight={`${(ratings[i - 1] / total) * 312}px`}
+                  />
+                </div>
+                <div className={styles.chart__xlabel}>
+                  <p className={styles.chart__xlabel__text}>{`${i}`}</p>
+                </div>
+              </div>
+            ) : (
+              <div
+                key={i}
+                className={styles.chart__bar__wrapper}
+                id={styles.chart__bar__wrapper__last}
+              >
+                <div className={styles.chart__bar__inner__wrapper}>
+                  <Bar
+                    highlight={maxIdx ? i === maxIdx + 1 : false}
+                    barHeight={`${(ratings[i - 1] / total) * 312}px`}
+                  />
+                </div>
+                <div className={styles.chart__xlabel}>
+                  <p className={styles.chart__xlabel__text}>{`${i}`}</p>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default StarRatingChart;
-
