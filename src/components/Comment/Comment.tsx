f9c5b355d2c5748416c56beee12b2aa796868544
yohan11 <yipco77@naver.com>
2023-08-22 14:39:14 +0900

    feat: 댓글 좋아요 기능 구현

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Comment.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -11,11 +11,62 @@ interface CommentProps {
 	commentContent: string;
 	commentComment: number;
 	commentLikes: number;
+	commentID: number;
 }
 
 const Comment: React.FC<{ commentProps: CommentProps }> = ({
 	commentProps,
 }) => {
+	const [isClicked, setIsClicked] = useState(false);
+	const [likeCount, setLikeCount] = useState(commentProps.commentLikes);
+
+	const postLike = async () => {
+		try {
+			// Replace with your API endpoint
+			const response = await fetch(
+				`http://52.78.121.235:8080/review/id/${commentProps.commentID}/like`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			if (response.ok) {
+				setIsClicked(true);
+				setLikeCount(likeCount + 1);
+			} else {
+				console.error("Failed to post like");
+			}
+		} catch (error) {
+			console.error("Error posting like:", error);
+		}
+	};
+
+	const postDislike = async () => {
+		try {
+			// Replace with your API endpoint
+			const response = await fetch(
+				`http://52.78.121.235:8080/review/id/${commentProps.commentID}/dislike`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			if (response.ok) {
+				setIsClicked(false);
+				setLikeCount(likeCount - 1);
+			} else {
+				console.error("Failed to post like");
+			}
+		} catch (error) {
+			console.error("Error posting like:", error);
+		}
+	};
 	return (
 		<div className={styles.comment__container}>
 			<div className={styles.comment__writer__info}>
@@ -35,15 +86,20 @@ const Comment: React.FC<{ commentProps: CommentProps }> = ({
 			</div>
 			<div className={styles.comment__reaction}>
 				<span>
-					<FontAwesomeIcon
-						icon={faHeart}
-						size="xl"
-						style={{
-							color: "#C0C0C0",
-						}}
-						className={styles.reaction_icon}
-					/>
-					{commentProps.commentLikes}
+					<a
+						href="javascript:void(0);"
+						onClick={isClicked ? postDislike : postLike}
+					>
+						<FontAwesomeIcon
+							icon={faHeart}
+							size="xl"
+							style={{
+								color: isClicked ? "red" : "#C0C0C0",
+							}}
+							className={styles.reaction_icon}
+						/>
+					</a>
+					{likeCount}
 				</span>
 				<span>
 					<FontAwesomeIcon
